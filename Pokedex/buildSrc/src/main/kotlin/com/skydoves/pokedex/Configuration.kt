22ae50f769fdfae203d146e14c2238d7b97637d8
Sahil Gupta <sahil865gupta@gmail.com>
2022-11-18 13:53:47 +0530

    chore: added pokedex app

---
+++ Pokedex/buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
@@ -0,0 +1,12 @@
+package com.skydoves.pokedex
+
+object Configuration {
+  const val compileSdk = 33
+  const val targetSdk = 33
+  const val minSdk = 23
+  const val majorVersion = 1
+  const val minorVersion = 1
+  const val patchVersion = 0
+  const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+  const val versionCode = 8
+}
